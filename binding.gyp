90873dae79272d1dfd274a6f6e23be295bbced68
Colin Riddell <colinriddell@codeplay.com>
2014-03-26 23:46:32 +0000

    Adding in OpenCL. can't find cl api functions at runtime. Is it even possible to call into shared libs from node gyp module?

--- binding.gyp
+++ binding.gyp
@@ -4,5 +4,13 @@
       "target_name": "hello",
       "sources": [ "src/hello.cpp" ]
     }
-  ]
+  ],
+
+'include_dirs': [
+  '/opt/AMDAPP/include',
+],
+'libraries': [
+  '-lOpenCL', '-L/opt/AMDAPP/lib/x86/lib'
+]
+
 }
--- src/hello.cpp
+++ src/hello.cpp
@@ -1,10 +1,30 @@
 #include <node.h>
 #include <v8.h>
 
+#include <CL/cl.h>
 using namespace v8;
 
 Handle<Value> Method(const Arguments& args) {
   HandleScope scope;
+
+  cl_device_id device_id;
+  cl_uint num_devices = 1u;
+  const cl_uint num_entries = 3;
+  cl_platform_id platforms;
+  cl_uint num_platforms = 0;
+  int errcode_ret =1;
+  
+  errcode_ret = clGetPlatformIDs(
+    num_entries,
+    &platforms,
+    &num_platforms);
+
+   errcode_ret = clGetDeviceIDs(platforms,
+    CL_DEVICE_TYPE_CPU,  //TODO: Make selectable for QPU
+    1u,
+    &device_id,
+    &num_devices);
+     
   return scope.Close(String::New("world"));
 }
 
